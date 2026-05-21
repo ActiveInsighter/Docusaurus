aee777706a76cdf422572ff5d98c0a76a2fd12d5
饿啊 <16424050+git-a@user.noreply.gitee.com>
2026-05-21 15:05:55 +0800

    fix: normalize site url and trailing slash

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -207,10 +207,11 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://to-any.top',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
+  trailingSlash: true,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
